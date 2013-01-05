0a43cac4584b6afbeaaed1fa546660c9418d0c9c
Erik Engbrecht <erik.engbrecht@gmail.com>
2013-01-05 18:59:36 -0500

    removed old files that are no longer applicable

--- src/main/scala/examples/erroneous.scala
+++ src/main/scala/examples/erroneous.scala
@@ -1,7 +1,6 @@
 package optional.examples
-//
-//object Erroneous extends optional.Application
-//{
-//  def main(times: Option[Int], greeting: Option[String], file: java.io.File) { }
-//  def main(dingle: Option[Int]) {}
-//}
+
+object Erroneous extends optional.Application {
+  def main(times: Option[Int], greeting: Option[String], file: java.io.File) { }
+  def main(dingle: Option[Int]) {}
+}
